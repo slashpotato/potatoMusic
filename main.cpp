759559c9630eda311135b1a915e17fecb3a28f72
slashpotato <slashpotato@proton.me>
2024-04-17 23:02:15 +0400

    indev commit ! use previous to build

--- main.cpp
+++ main.cpp
@@ -17,38 +17,122 @@
 #include <QMessageBox>
 #include <QPixmap>
 #include <QFileInfo>
+#include <QStringList>
+#include <QSlider>
 
 // taglib
 #include <taglib/tag.h>
 #include <taglib/fileref.h>
-#include <taglib/mpegfile.h>
-#include <taglib/flacfile.h>
-#include <taglib/opusfile.h>
-#include <taglib/oggfile.h>
-#include <taglib/wavfile.h>
 #include <taglib/id3v2tag.h>
 #include <taglib/id3v2frame.h>
 #include <taglib/attachedpictureframe.h>
+#include <taglib/tpropertymap.h>
+#include <taglib/mpegfile.h>
+#include <taglib/flacfile.h>
+#include <taglib/oggfile.h>
+#include <taglib/vorbisfile.h>
+#include <taglib/wavfile.h>
+#include <taglib/mp4file.h>
+#include <taglib/opusfile.h>
 
-QString version = "0.0.7";
+#include <queue>
+
+QString version = "0.1.2";
+
+class MusicQueue
+{
+public:
+    void addToQueue(const QString &filePath)
+    {
+        queue.push_back(filePath); // Используем push_back для добавления элемента в конец очереди
+    }
+
+    QString removeFromQueue()
+    {
+        if (!queue.empty())
+        {
+            QString filePath = queue.front();
+            queue.pop_front(); // Используем pop_front для удаления элемента из начала очереди
+            return filePath;
+        }
+        return QString();
+    }
+
+    QStringList getQueue() const
+    {
+        return QStringList(queue.begin(), queue.end());
+    }
+
+    bool isQueueEmpty() const
+    {
+        return queue.empty();
+    }
+
+
+private:
+    std::deque<QString> queue;
+};
+
+MusicQueue musicQueue;
+
+QSlider* createSlider(QWidget *parent) {
+    QSlider *slider = new QSlider(Qt::Horizontal, parent);
+    slider->setMinimum(0);
+    slider->setMaximum(100);
+    slider->setValue(0);
+    return slider;
+}
 
 // defining global elements
-QMediaPlayer* player = nullptr;
-QAudioOutput* audioOutput = nullptr;
-QPushButton* bplay = nullptr;
-QImage* songimage = nullptr;
-QImage* tmpimage = nullptr;
-QLabel* songImageLabel = nullptr;
-QLabel* songname = nullptr;
-QLabel* songauthor = nullptr;
+QMediaPlayer *player = nullptr;
+QAudioOutput *audioOutput = nullptr;
+QPushButton *bplay = nullptr;
+QImage *songimage = nullptr;
+QImage *tmpimage = nullptr;
+QLabel *songImageLabel = nullptr;
+QLabel *songname = nullptr;
+QLabel *songauthor = nullptr;
+QSlider *slider = nullptr;
 
 int thumbScale = 300;
 bool nowPaused = false;
 
-void onNext(); void onPrevious(); void onPause(); void open(); void setVolume(); void open(); void ohelp();
+void onNext();
+void onPrevious();
+void onPause();
+void open();
+void setVolume();
+void open();
+void ohelp();
+void opref();
+void viewMusic();
+
+void setTrackPositionFun(int position) {
+    qint64 duration = player->duration();
+    if (duration > 0) {
+        qint64 newPosition = (position * duration) / 100;
+        player->setPosition(newPosition);
+    }
+}
+
+void onPositionChanged(qint64 position) {
+    qreal pos = static_cast<qreal>(position);
+    qreal duration = static_cast<qreal>(player->duration());
+
+    qreal progressPercentage = (pos / duration) * 100.0;
+
+    if (!slider->isSliderDown()) {
+       slider->setValue(progressPercentage);
+    }
+
+    if (position >= duration) {
+       onPause();
+    }
+}
 
 // main
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // application
     QApplication app(argc, argv);
     app.setOrganizationDomain("slshptt.vercel.app");
@@ -140,19 +224,28 @@ int main(int argc, char *argv[]) {
     player->setAudioOutput(audioOutput);
     audioOutput->setVolume(1);
 
+    // slider
+    slider = new QSlider(Qt::Horizontal);
+    slider->setMinimum(0);
+    slider->setMaximum(100);
+    slider->setValue(0);
+
     // connecting
     QObject::connect(bprev, &QPushButton::clicked, onPrevious);
     QObject::connect(bplay, &QPushButton::clicked, onPause);
     QObject::connect(bnext, &QPushButton::clicked, onNext);
     QObject::connect(asetv, &QAction::triggered, setVolume);
-    //QObject::connect(tpref, &QAction::triggered, pref);
+    QObject::connect(tpref, &QAction::triggered, opref);
     QObject::connect(thelp, &QAction::triggered, ohelp);
     QObject::connect(openmusic, &QAction::triggered, open);
+    QObject::connect(viewmusic, &QAction::triggered, viewMusic);
+    QObject::connect(slider, &QSlider::sliderMoved, setTrackPositionFun);
+    QObject::connect(player, &QMediaPlayer::positionChanged, onPositionChanged);
 
     // layout
     QHBoxLayout *playout = new QHBoxLayout(widget); // for hpadding
-    QVBoxLayout *vlayout = new QVBoxLayout; // for vpadding
-    QGridLayout *glayout = new QGridLayout; // for grid
+    QVBoxLayout *vlayout = new QVBoxLayout;         // for vpadding
+    QGridLayout *glayout = new QGridLayout;         // for grid
     playout->setMenuBar(toolbar);
 
     // do hpadding
@@ -171,8 +264,9 @@ int main(int argc, char *argv[]) {
     glayout->addWidget(bnext, 1, 2);
 
     glayout->addWidget(songImageLabel, 0, 0, 1, 3);
-    glayout->addWidget(songname, 2, 0, 1, 3);
-    glayout->addWidget(songauthor, 3, 0, 1, 3);
+    glayout->addWidget(slider, 2, 0, 1, 3);
+    glayout->addWidget(songname, 3, 0, 1, 3);
+    glayout->addWidget(songauthor, 4, 0, 1, 3);
 
     // run app
     widget->show();
@@ -181,27 +275,33 @@ int main(int argc, char *argv[]) {
 }
 
 // media buttons
-void onNext() {
+void onNext()
+{
     QMessageBox msgBox;
     msgBox.setText("next");
     msgBox.exec();
 }
 
-void onPrevious() {
+void onPrevious()
+{
     QMessageBox msgBox;
     msgBox.setText("previous");
     msgBox.exec();
 }
 
-void onPause() {
+void onPause()
+{
     QIcon ipause = QIcon::fromTheme("media-playback-pause");
     QIcon iplay = QIcon::fromTheme("media-playback-start");
 
-    if (nowPaused == true) {
+    if (nowPaused == true)
+    {
         player->play();
         bplay->setIcon(ipause);
         nowPaused = false;
-    } else {
+    }
+    else
+    {
         player->pause();
         bplay->setIcon(iplay);
         nowPaused = true;
@@ -209,7 +309,16 @@ void onPause() {
 }
 
 // action buttons
-void setVolume() {
+void viewMusic() {
+    QString queueList;
+    for (const QString &song : musicQueue.getQueue()) {
+        queueList += song + "\n";
+    }
+    QMessageBox::information(nullptr, "Queue", queueList);
+}
+
+void setVolume()
+{
     bool ok;
     int min = 0, max = 100, value = 100, step = 10;
     float value_int = QInputDialog::getInt(nullptr, "0-100", "Enter new volume:", value, min, max, step, &ok);
@@ -217,82 +326,139 @@ void setVolume() {
     audioOutput->setVolume(res);
 }
 
-void ohelp() {
+void ohelp()
+{
     QMessageBox msgBox;
-    msgBox.setText("potatoMusic v"+version+"\n\nShortcuts:\nCtrl+O - open\nCtrl+V - set volume level\n\nhttps://github.com/slashpotato/potatoMusic");
+    msgBox.setText("potatoMusic v" + version + "\n\nShortcuts:\nCtrl+O - open\nCtrl+V - set volume level\n\nhttps://github.com/slashpotato/potatoMusic");
     msgBox.exec();
 }
+void opref()
+{
+    //QMessageBox msgBox;
+    //msgBox.setText();
+    //msgBox.exec();
+}
 
-void open() {
+void open()
+{
     QFileDialog dialog;
-    dialog.setFileMode(QFileDialog::ExistingFile);
+    dialog.setFileMode(QFileDialog::ExistingFiles);
     dialog.setViewMode(QFileDialog::Detail);
-    dialog.setNameFilters({"Audio Files (*.mp3 *.ogg *.wav *.flac *.opus)", "Any files (*)"});
+    dialog.setNameFilters({"Audio Files (*.mp3 *.ogg *.wav *.flac *.opus *.m4a)", "Any files (*)"});
 
-    if (dialog.exec()) {
+    if (dialog.exec())
+    {
         QStringList fileNames = dialog.selectedFiles();
         QString filePath = fileNames.first();
-        player->setSource(filePath);
-
-        // get album art
-        QFileInfo fileInfo(filePath);
-        QString ext = fileInfo.suffix().toLower();
-
-        if (ext == "mp3") {
-            TagLib::MPEG::File mp3File(filePath.toUtf8().constData());
-            if (!mp3File.isValid()) {
-                QMessageBox::warning(nullptr, "Error", "Invalid MP3 file.");
-                return;
-            }
-
-            const TagLib::ID3v2::FrameList &frameList = mp3File.ID3v2Tag()->frameListMap()["APIC"];
-            if (!frameList.isEmpty()) {
-                TagLib::ID3v2::AttachedPictureFrame *frame = dynamic_cast<TagLib::ID3v2::AttachedPictureFrame *>(frameList.front());
-                if (frame) {
-                    TagLib::ByteVector imageData = frame->picture();
-                    QPixmap pixmap;
-                    pixmap.loadFromData(reinterpret_cast<const uchar *>(imageData.data()), imageData.size());
-                    if (!pixmap.isNull()) {
-                        songImageLabel->setPixmap(pixmap.scaled(thumbScale, thumbScale, Qt::KeepAspectRatio));
-                    }
-                }
-            }
-
-            QString title = QString::fromStdString(mp3File.tag()->title().toCString(true));
-            QString artist = QString::fromStdString(mp3File.tag()->artist().toCString(true));
-            songname->setText(title);
-            songauthor->setText(artist);
-        } else if (ext == "flac") {
-            TagLib::FLAC::File flacFile(filePath.toUtf8().constData());
-            if (!flacFile.isValid()) {
-                QMessageBox::warning(nullptr, "Error", "Invalid FLAC file.");
-                return;
-            }
-
-            const TagLib::List<TagLib::FLAC::Picture *> &pictureList = flacFile.pictureList();
-            if (!pictureList.isEmpty()) {
-                const TagLib::FLAC::Picture *picture = pictureList.front();
-                if (picture) {
-                    TagLib::ByteVector imageData = picture->data();
-                    QPixmap pixmap;
-                    pixmap.loadFromData(reinterpret_cast<const uchar *>(imageData.data()), imageData.size());
-                    if (!pixmap.isNull()) {
-                        songImageLabel->setPixmap(pixmap.scaled(thumbScale, thumbScale, Qt::KeepAspectRatio));
-                    }
-                }
-            }
-
-            QString title = QString::fromStdString(flacFile.tag()->title().toCString(true));
-            QString artist = QString::fromStdString(flacFile.tag()->artist().toCString(true));
-            songname->setText(title);
-            songauthor->setText(artist);
-        } else {
-            QMessageBox::warning(nullptr, "Warning", "Unsupported file format: metadata won't be loaded, but most likely played."); // :)
-            return;
+        for (const QString& filePath : fileNames)
+        {
+            // Добавить каждый выбранный файл в очередь воспроизведения
+            musicQueue.addToQueue(filePath);
         }
+        // Если очередь не пуста и воспроизведение не начато, начать воспроизведение
+        if (!musicQueue.isQueueEmpty() && !player->isPlaying()) {
+            QString firstSong = musicQueue.removeFromQueue();
+            player->setSource(firstSong);
 
-        if (nowPaused) {
-            onPause();
+            // get album art
+            QFileInfo fileInfo(filePath);
+            QString ext = fileInfo.suffix().toLower();
+
+            if (nowPaused)
+            {
+                onPause();
+            }
+
+            if (ext == "mp3")
+            {
+                TagLib::MPEG::File mp3File(filePath.toUtf8().constData());
+                if (!mp3File.isValid())
+                {
+                    QMessageBox::warning(nullptr, "Error", "Invalid MP3 file.");
+                    return;
+                }
+
+                const TagLib::ID3v2::FrameList &frameList = mp3File.ID3v2Tag()->frameListMap()["APIC"];
+                if (!frameList.isEmpty())
+                {
+                    TagLib::ID3v2::AttachedPictureFrame *frame = dynamic_cast<TagLib::ID3v2::AttachedPictureFrame *>(frameList.front());
+                    if (frame)
+                    {
+                        TagLib::ByteVector imageData = frame->picture();
+                        QPixmap pixmap;
+                        pixmap.loadFromData(reinterpret_cast<const uchar *>(imageData.data()), imageData.size());
+                        if (!pixmap.isNull())
+                        {
+                            songImageLabel->setPixmap(pixmap.scaled(thumbScale, thumbScale, Qt::KeepAspectRatio));
+                        }
+                    }
+                }
+
+                QString title = QString::fromStdString(mp3File.tag()->title().toCString(true));
+                QString artist = QString::fromStdString(mp3File.tag()->artist().toCString(true));
+                songname->setText(title);
+                songauthor->setText(artist);
+            }
+            else if (ext == "flac")
+            {
+                TagLib::FLAC::File flacFile(filePath.toUtf8().constData());
+                if (!flacFile.isValid())
+                {
+                    QMessageBox::warning(nullptr, "Error", "Invalid FLAC file.");
+                    return;
+                }
+
+                const TagLib::List<TagLib::FLAC::Picture *> &pictureList = flacFile.pictureList();
+                if (!pictureList.isEmpty())
+                {
+                    const TagLib::FLAC::Picture *picture = pictureList.front();
+                    if (picture)
+                    {
+                        TagLib::ByteVector imageData = picture->data();
+                        QPixmap pixmap;
+                        pixmap.loadFromData(reinterpret_cast<const uchar *>(imageData.data()), imageData.size());
+                        if (!pixmap.isNull())
+                        {
+                            songImageLabel->setPixmap(pixmap.scaled(thumbScale, thumbScale, Qt::KeepAspectRatio));
+                        }
+                    }
+                }
+
+                QString title = QString::fromStdString(flacFile.tag()->title().toCString(true));
+                QString artist = QString::fromStdString(flacFile.tag()->artist().toCString(true));
+                songname->setText(title);
+                songauthor->setText(artist);
+            }
+            else if (ext == "ogg" || ext == "wav" || ext == "m4a" || ext == "opus")
+            {
+                TagLib::FileRef fileRef(filePath.toUtf8().constData());
+                if (!fileRef.isNull() && fileRef.tag())
+                {
+                    TagLib::Tag *tag = fileRef.tag();
+                    QString title = QString::fromStdString(tag->title().toCString(true));
+                    QString artist = QString::fromStdString(tag->artist().toCString(true));
+                    songname->setText(title);
+                    songauthor->setText(artist);
+
+                    TagLib::PropertyMap properties = tag->properties();
+                    if (properties.contains("WM/Picture"))
+                    {
+                        TagLib::String pictureData = properties["WM/Picture"].toString();
+                        QString pictureDataQString = QString::fromStdString(pictureData.to8Bit(true).data());
+                        QPixmap pixmap;
+                        pixmap.loadFromData(reinterpret_cast<const uchar *>(pictureDataQString.toUtf8().constData()), pictureDataQString.length());
+                        if (!pixmap.isNull())
+                        {
+                            songImageLabel->setPixmap(pixmap.scaled(thumbScale, thumbScale, Qt::KeepAspectRatio));
+                        }
+                    }
+                }
+            }
+            else
+            {
+                QMessageBox::warning(nullptr, "Warning", "Unsupported file format: metadata won't be loaded, but most likely played."); // :)
+                return;
+            }
         }
     }
 }
