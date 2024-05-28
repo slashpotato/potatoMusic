328e74923993ca7a794892a7d752e86f3392ec73
slashpotato <slashpotato@proton.me>
2024-05-28 14:27:31 +0400

    updated update script

--- update.sh
+++ update.sh
@@ -3,7 +3,14 @@
 git fetch
 
 if git status | grep 'behind' > /dev/null; then
+  echo "Update found."
+  echo ""
   git pull
+  if [ -f $PWD/potatoMusic ]; then
+    echo "Binary found and will be re-built."
+    echo ""
+    ./build.sh
+  fi
+else
+  echo "No updates found."
 fi
-
-./build.sh
