df4ba7096212089c29790014cdbbadea091eae61
slashpotato <slashpotato@proton.me>
2024-06-06 15:58:44 +0400

    clean script update

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 qmake6 || qmake-qt6
 make clean &> /dev/null
-rm -rf main.o mainwindow.o moc_mainwindow.cpp moc_mainwindow.o moc_predefs.h potatoMusic nohup.out clean
+rm -rf main.o mainwindow.o moc_mainwindow.cpp moc_mainwindow.o moc_predefs.h potatoMusic nohup.out clean pkg src potatoMusic*.tar.*
