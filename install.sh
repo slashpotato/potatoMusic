4580de19dff0fab71fcacbbf97c82721934b815d
slashpotato <slashpotato@proton.me>
2024-08-05 22:24:41 +0400

    changed scripts

--- install.sh
+++ install.sh
@@ -5,5 +5,14 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-make install --jobs=$(grep -c ^processor /proc/cpuinfo)
-cp *.desktop /usr/local/share/applications
+if [ -f /bin/xbps-install ]; then
+        xbps-install -yu ffmpeg-devel taglib-devel ffmpeg taglib qt6-multimedia qt6-multimedia-devel qt6-core qt6-base qt6-base-devel qt6-widgets qt6-gui &> /dev/null
+elif [ -f /bin/apt ]; then
+        apt install -y qt6-multimedia-dev qt6-base-dev ffmpeg libtag1-dev &> /dev/null
+else
+        echo "Plese install dependencies by yourself. Your package manager is not supported."
+fi
+
+qmake6 || qmake-qt6 || /lib/qt6/bin/qmake6
+make install --jobs=$(grep -c ^processor /proc/cpuinfo) &> /dev/null && echo "Successfully installed" || echo "Error"
+cp *.desktop /usr/local/share/applications && echo "Created desktop entry"
--- uninstall.sh
+++ uninstall.sh
@@ -5,5 +5,5 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-make uninstall --jobs=$(grep -c ^processor /proc/cpuinfo)
-rm -rf /usr/local/share/applications/$(find . | grep .desktop | cut -c 3-)
+make uninstall --jobs=$(grep -c ^processor /proc/cpuinfo) && echo "Successfully uninstalled" || echo "Error"
+rm -rf /usr/local/share/applications/$(find . | grep .desktop | cut -c 3-) && echo "Deleted desktop entry"
